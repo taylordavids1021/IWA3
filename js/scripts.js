6bb9b3a96bcb7735072e014e7d7d2cdcd9ae82e7
Taylor <taylordavids997@gmail.com>
2023-08-02 14:43:11 +0200

    import and innerText message to html

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,6 @@
 import { company } from './configuration.js'
 import { year } from './configuration.js'
 
-const message = '© ' + company +  year ;
+const message = '© ' + company +  year 
 
-document.querySelector("footer").innerHTML = message ;
+document.querySelector("footer").innerText = message 
